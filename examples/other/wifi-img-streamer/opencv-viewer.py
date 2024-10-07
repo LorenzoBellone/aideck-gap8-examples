3ad706dc8a01a712aacca75dc75d28e82a42a10d
LorenzoBellone <lorenzo.bellone@outlook.it>
2024-10-07 14:14:44 +0400

    check on first info bytes

--- examples/other/wifi-img-streamer/opencv-viewer.py
+++ examples/other/wifi-img-streamer/opencv-viewer.py
@@ -72,56 +72,61 @@ import cv2
 start = time.time()
 count = 0
 
+
 while(1):
     # First get the info
     packetInfoRaw = rx_bytes(4)
-    #print(packetInfoRaw)
-    [length, routing, function] = struct.unpack('<HBB', packetInfoRaw)
-    #print("Length is {}".format(length))
-    #print("Route is 0x{:02X}->0x{:02X}".format(routing & 0xF, routing >> 4))
-    #print("Function is 0x{:02X}".format(function))
+    # print(f"Received new info packet: {packetInfoRaw}")
+    if packetInfoRaw[:3] == b'\r\x00c':
+      # print(f"THe packet is good!")
+      [length, routing, function] = struct.unpack('<HBB', packetInfoRaw)
+      # print("Length is {}".format(length))
+      # print("Route is 0x{:02X}->0x{:02X}".format(routing & 0xF, routing >> 4))
+      # print("Function is 0x{:02X}".format(function))
 
-    imgHeader = rx_bytes(length - 2)
-    #print(imgHeader)
-    #print("Length of data is {}".format(len(imgHeader)))
-    [magic, width, height, depth, format, size] = struct.unpack('<BHHBBI', imgHeader)
+      imgHeader = rx_bytes(length - 2)
+      # print(imgHeader)
+      # print("Length of data is {}".format(len(imgHeader)))
+      # if len(imgHeader) == 11:
+      [magic, width, height, depth, format, size] = struct.unpack('<BHHBBI', imgHeader)
 
-    if magic == 0xBC:
-      #print("Magic is good")
-      #print("Resolution is {}x{} with depth of {} byte(s)".format(width, height, depth))
-      #print("Image format is {}".format(format))
-      #print("Image size is {} bytes".format(size))
+      if magic == 0xBC:
+        #print("Magic is good")
+        #print("Resolution is {}x{} with depth of {} byte(s)".format(width, height, depth))
+        #print("Image format is {}".format(format))
+        #print("Image size is {} bytes".format(size))
 
-      # Now we start rx the image, this will be split up in packages of some size
-      imgStream = bytearray()
+        # Now we start rx the image, this will be split up in packages of some size
+        imgStream = bytearray()
 
-      while len(imgStream) < size:
-          packetInfoRaw = rx_bytes(4)
-          [length, dst, src] = struct.unpack('<HBB', packetInfoRaw)
-          #print("Chunk size is {} ({:02X}->{:02X})".format(length, src, dst))
-          chunk = rx_bytes(length - 2)
-          imgStream.extend(chunk)
-     
-      count = count + 1
-      meanTimePerImage = (time.time()-start) / count
-      print("{}".format(meanTimePerImage))
-      print("{}".format(1/meanTimePerImage))
+        while len(imgStream) < size:
+            packetInfoRaw = rx_bytes(4)
+            [length, dst, src] = struct.unpack('<HBB', packetInfoRaw)
+            #print("Chunk size is {} ({:02X}->{:02X})".format(length, src, dst))
+            chunk = rx_bytes(length - 2)
+            imgStream.extend(chunk)
+      
+        count = count + 1
+        meanTimePerImage = (time.time()-start) / count
+        print("{}".format(meanTimePerImage))
+        print("{}".format(1/meanTimePerImage))
 
-      if format == 0:
-          bayer_img = np.frombuffer(imgStream, dtype=np.uint8)   
-          bayer_img.shape = (244, 324)
-          color_img = cv2.cvtColor(bayer_img, cv2.COLOR_BayerBG2BGRA)
-          cv2.imshow('Raw', bayer_img)
-          cv2.imshow('Color', color_img)
-          if args.save:
-              cv2.imwrite(f"stream_out/raw/img_{count:06d}.png", bayer_img)
-              cv2.imwrite(f"stream_out/debayer/img_{count:06d}.png", color_img)
-          cv2.waitKey(1)
-      else:
-          with open("img.jpeg", "wb") as f:
-              f.write(imgStream)
-          nparr = np.frombuffer(imgStream, np.uint8)
-          decoded = cv2.imdecode(nparr,cv2.IMREAD_UNCHANGED)
-          cv2.imshow('JPEG', decoded)
-          cv2.waitKey(1)
+        if format == 0:
+            if len(imgStream) == 244*324:
+              bayer_img = np.frombuffer(imgStream, dtype=np.uint8)   
+              bayer_img.shape = (244, 324)
+              # color_img = cv2.cvtColor(bayer_img, cv2.COLOR_BayerBG2BGRA)
+              cv2.imshow('Raw', bayer_img)
+              # cv2.imshow('Color', color_img)
+              if args.save:
+                  cv2.imwrite(f"stream_out/raw/img_{count:06d}.png", bayer_img)
+                  # cv2.imwrite(f"stream_out/debayer/img_{count:06d}.png", color_img)
+              cv2.waitKey(1)
+        else:
+            with open("img.jpeg", "wb") as f:
+                f.write(imgStream)
+            nparr = np.frombuffer(imgStream, np.uint8)
+            decoded = cv2.imdecode(nparr,cv2.IMREAD_UNCHANGED)
+            cv2.imshow('JPEG', decoded)
+            cv2.waitKey(1)
 
